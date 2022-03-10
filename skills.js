213bdcb8a2f50fd1b54e8010e7351fb7401f648a
Ambika <amika@aptonworks.com>
2022-03-10 16:22:38 +0530

    progress in bootstrap

---
+++ skills.js
@@ -0,0 +1,10 @@
+function button(){
+    var click=document.getElementById("Button1")
+    if(click.style.display==="none"){
+        click.style.display="block";
+    }
+    else
+    {
+        click.style.display="none";
+    }
+}
